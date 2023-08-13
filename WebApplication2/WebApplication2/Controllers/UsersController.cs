33565ea36cf43070fdec4b00b88b27e08571fe91
Tatjana Ristovska <96683871+Ulicharche@users.noreply.github.com>
2023-08-13 14:55:33 +0200

    Domashna 1

---
+++ WebApplication2/WebApplication2/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> GetAllUsers()
+        {
+            return Ok(StaticDb.UsersName);
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<string> GetUserByIndex(int index)
+        {
+            try
+            {
+                if (index < StaticDb.UsersName.Count)
+                {
+                    return BadRequest("Indeksot ima negativna vrednost");
+                }
+                if (index >= StaticDb.UsersName.Count)
+                {
+                    return NotFound($"{index}");
+                }
+                return Ok(StaticDb.UsersName[index]);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
+
---
+++ WebApplication2/WebApplication2/StaticDb.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication2
+{
+    public static class StaticDb
+    {
+        public static List<string> UsersName = new()
+        {
+            "Tanja",
+            "Andrej",
+            "Matej",
+            "Angel"
+        };
+    }
+}
