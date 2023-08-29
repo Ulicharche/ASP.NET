f4678fa4a86d87760ebcb128d654b5ee15ab9eac
Tatjana Ristovska <96683871+Ulicharche@users.noreply.github.com>
2023-08-29 20:09:45 +0200

    Add files via upload

---
+++ WebApplication1/WebApplication1/Controllers/BooksController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+
+
+    namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]   
+    public class BooksController : ControllerBase
+    {
+        [HttpGet()]
+        public ActionResult <Book> GetBook(int? index)
+        { 
+            try
+            {
+                if (index == null)
+                {
+                    return Ok(StaticDb.ListBooks);
+                }
+                if(index<0)
+                {
+                    return BadRequest("Indeksot e so negativna vrednost!");
+                }
+                if (index>= StaticDb.ListBooks.Count())
+                {
+                    return NotFound($"Ne go najdovme indeksot {index}");
+                }
+                return Ok(StaticDb.ListBooks[index.Value]);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,ex.Message);
+            }
+        }
+
+        [HttpGet("filterBooks")]
+        public ActionResult<Book> FilterBooks(string? author, string? title)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(author) || string.IsNullOrEmpty(title))
+                {
+                    return BadRequest("Vnesete gi potrebnite podatoci");
+                }
+                return Ok(StaticDb.ListBooks.FirstOrDefault(x => x.Author.ToLower() == author.ToLower() && x.Title.ToLower() == title.ToLower()));
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,ex.Message);
+            }
+        }
+
+        [HttpPost] 
+        public IActionResult AddNewBook([FromBody] Book book)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(book.Author) || string.IsNullOrEmpty(book.Title))
+                {
+                    return BadRequest("Dodajte naslov i avtor na knigata!");
+                }
+                StaticDb.ListBooks.Add(book);
+                return StatusCode(StatusCodes.Status201Created, $"Knigata {book.Author} od {book.Title}e dodadena");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+
+            }
+        }
+        [HttpPost("listOfBooks")]
+        public ActionResult<List<string>> ListOfBooks([FromBody]List<Book>Books) 
+        {
+            if (!Books.Any())
+            {
+                return BadRequest("Pratete ni lista od knigi");
+            }
+
+            List<string> result = new List<string>();
+            foreach (Book book in Books)
+            {
+                result.Add(book.Title);
+            }
+            return Ok(result);  
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/StaticDb.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class StaticDb
+    {
+        public static List<Book> ListBooks = new List<Book>
+        {
+            new Book()
+            {
+                Author = "Sebastijan Ficek",
+                Title = "Pacientot"
+            },
+            new Book()
+            {
+                Author = "Lusinda Rajli",
+                Title = "Sedumte sestri"
+            },
+            new Book()
+            {
+                Author = "Dzon Shorz",
+                Title = "Pod Mermerno nebo"
+            },
+            new Book()
+            {
+                Author = "Kolin Huver",
+                Title = "Ova zavrshuva so nas"
+            },
+            new Book()
+            {
+                Author = "Dona Kroz",
+                Title = "Papata Jovana"
+            }
+        };
+    }
+}
